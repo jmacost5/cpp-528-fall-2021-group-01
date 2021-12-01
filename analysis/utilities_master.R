0c6c74663dac4e105c63b15483673d671f3f56c3
AprilPeck <apes2733@aim.com>
2021-11-30 22:42:26 -0700

    Lab 03 corrections
    Added choropleth map to Lab 03 (final step) and removed some code so it's more appropriate for the final deliverable. This included adding lab 03 code to utilities_master. I pulled the current utilities_master from the main branch, so everything should be in there. Feel free to compare to what's currently in main to be sure I didn't miss anything. Uploading utilities_master, create-dorling-amp.r, lab-03-PECK_final.html and lab-03-PECK_final.rmd.
    
    Removed sample photos from assets/img/about and corrected filename of my photo.

--- analysis/utilities_master.R
+++ analysis/utilities_master.R
@@ -5,6 +5,9 @@
 # Purpose:  Create a function that will be sourced within another file
 #
 
+
+# Lab 02 Functions--------------------------------------------------------------
+
 # create function ----
 today <- function() {
   # store today's date
@@ -20,12 +23,10 @@ today <- function() {
 
 # filter data by category ----
 
-category_search <- function( data, search_term )
-{
-  keyword.search <- grepl( search_term, data$category, ignore.case = TRUE)
-  data.category <- data[ keyword.search, c("root", "root2", "category", "definition") ]
-  
-  return(data.category)
+cat_filter <- function(data , search.cat ){
+  cat_filter <- data%>%
+    filter(category == search.cat )
+  return(cat_filter)
 }
 
 # TWO: Create a function that searches variable descriptions for a specific string and returns 
@@ -34,12 +35,10 @@ category_search <- function( data, search_term )
 
 # search for data with keyword ----
 
-search_word <- function( data, keyword )
-{
-  keyword.search <- grepl( keyword, data$definition, ignore.case = TRUE)
-  data.keywords <- data[ keyword.search, c("root", "root2", "category", "definition")]
-  
-  return( data.keywords)
+search_description <- function(data , keywords) {
+  search_description <- grepl(keywords , data$definition , ignore.case =T )
+  search.keywords <- data[search_description , c("root","root2", "category" ,"definition" )]
+  return(search.keywords)
 }
 
 
@@ -61,15 +60,73 @@ search_word <- function( data, keyword )
 search_years <- function(data, years)
 {
   
+  # set column names to lowercase
+  colnames(data) <- tolower(colnames(data))
+  
+  # set variables to lowercase
+  data <- mutate_all(data, .funs = tolower)
+  
+  # remove empty strings and white spaces
+  data <- data[!grepl("^\\s+$", data)]
+  
+  # replace missing values with NA
+  data[ data == ""] <- NA
+  
+  # select root, root2, category, and definition columns to keep in data set
+  columns <- c("root", "root2", "category", "definition")
+  
+  #list strings that we want to remove from our time period column names
+  prefix <- c("x")
+  suffix <- c(".f", ".s")
+  
+  # select data to keep
   df.years <- data %>%
     select(root, root2, category, definition, contains(years))
   
-  return(df.years)
+  #subset data to keep root, root2, and the columns that contain the names of our time period
+  #data_cleaned <- data[, c(grepl( paste( c( time.periods, columns), collapse = "|"), colnames(data) )) ]
+  
+  # remove prefix and suffix from column names
+  names <- colnames(df.years) %>%
+    str_remove(prefix) %>%
+    str_remove(suffix)
+  
+  # rename columns
+  colnames(df.years) <- names
+  
+  # change years columns to long
+  df.years.long <- df.years %>% pivot_longer( !columns, names_to = "year", values_to = "variable" ) %>% 
+    na.omit() %>%
+    distinct( root, year, variable)
+  
+  # create data sets of distinct root and year variables and combine for reference of all possible root/year combinations
+  root <- df.years.long %>% distinct(root) %>% select(root)
+  year <- df.years.long %>% distinct(year) %>% select(year)
+  root_list <- crossing( root, year )
+  
+  # Left join root_list and df.years.long to identify variables for each year.
+  root_variables <- left_join( root_list, df.years.long, by = c( "root", "year"))
+  
+  # List of roots with missing variables
+  root_missing <- root_variables %>% filter( is.na( variable ) )
+  
+  # create final dataset
+  data_final <- subset(df.years.long, !(df.years.long$root %in% root_missing$root))
+  
+  # join back to original dataset to add category and definition (and verify accuracy)
+  data_final <- left_join(data_final, data, by = "root") %>%
+    select(root, year, variable, category, definition)
+  
+  return(data_final)
+  
   
 }
 
+# End of Lab 02 Functions--------------------------------------------------------------
 
-### Lab 03 Functions
+
+# Lab 03 Functions--------------------------------------------------------------
+
 build_year <- function( fn1, fn2, year )
 {
   
@@ -254,3 +311,176 @@ jplot <- function( x1, x2, lab1="", lab2="", draw.line=T, ... )
     lines( lowess(x2[ok]~x1[ok]), col="red", lwd=3 ) }
   
 }
+
+
+# Wrangle data to create shapefile for chloropleth map
+# load data
+crosswalk <- read.csv( "https://raw.githubusercontent.com/DS4PS/cpp-529-master/master/data/cbsatocountycrosswalk.csv",  stringsAsFactors=F, colClasses="character" )
+
+# search for cities names by strings, use the ^ anchor for "begins with" 
+
+grep( "^SAN FRAN", crosswalk$msaname, value=TRUE ) 
+these.sf <- crosswalk$msaname == "SAN FRANCISCO, CA"   # Find San Francisco (T/F)
+these.fips <- crosswalk$fipscounty[ these.sf ]         # Find SF counties
+these.fips <- na.omit( these.fips )
+
+state.fips <- substr( these.fips, 1, 2 )                    # Substring state code
+county.fips <- substr( these.fips, 3, 5 )                   # substring county codes
+
+sf.pop <-
+  get_acs( geography = "tract", variables = "B01003_001",
+           state = "06", county = county.fips[state.fips=="06"], geometry = TRUE ) %>% 
+  select( GEOID, estimate ) %>%
+  rename( POP=estimate )
+#sanfran.pop$GEOID<-substr(sanfran.pop$GEOID,2,length(sf.pop$GEOID)) # remove leading 0's
+
+
+
+
+# End of Lab 03 Functions--------------------------------------------------------------
+
+# Lab 04 Functions--------------------------------------------------------------
+
+# load necessary packages ----
+`%>%` <- magrittr::`%>%`
+
+# load necessary constants ----
+
+# stargazer settings
+S_TYPE <- "text"
+
+# inflation rate
+INFLATION_RATE <- 1.28855 
+
+# load custom functions ----
+
+# Helper functions for the **pairs()** correlation table 
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y))
+  txt <- format(c(r, 0.123456789), digits=digits)[1]
+  txt <- paste(prefix, txt, sep="")
+  if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
+  
+  test <- cor.test(x,y)
+  # borrowed from printCoefmat
+  Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
+                   cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                   symbols = c("***", "**", "*", ".", " "))
+  
+  text(0.5, 0.5, txt, cex = 1.5 )
+  text(.7, .8, Signif, cex=cex, col=2)
+}
+
+panel.smooth <- function (x, y, col = par("col"), bg = NA, pch = par("pch"), 
+                          cex = 0.5, col.smooth = "red", span = 2/3, iter = 3, ...) 
+{
+  points(x, y, pch = 19, col = gray(0.7,0.2), bg = bg, cex = cex)
+  ok <- is.finite(x) & is.finite(y)
+  if (any(ok)) 
+    lines(stats::lowess(x[ok], y[ok], f = span, iter = iter), 
+          col = col.smooth, lwd=2, ...)
+}
+
+# custom plot
+jplot <- function( x1, x2, lab1="", lab2="", draw.line=T, ... )
+{
+  
+  plot( x1, x2,
+        pch=19, 
+        col=gray(0.6, alpha = 0.2), 
+        cex=0.5,  
+        bty = "n",
+        xlab=lab1, 
+        ylab=lab2, cex.lab=1.5,
+        ... )
+  
+  if( draw.line==T ){ 
+    ok <- is.finite(x1) & is.finite(x2)
+    lines( lowess(x2[ok]~x1[ok]), col="red", lwd=3 ) }
+  
+}
+
+# load necessary data ----
+# remember to use the here::here() function
+d1 <- readRDS( here::here( "data/rodeo/LTDB-2000.rds" ) )
+d2 <- readRDS( here::here( "data/rodeo/LTDB-2010.rds" ) )
+md <- readRDS( here::here( "data/rodeo/LTDB-META-DATA.rds" ) )
+
+d1 <- dplyr::select( d1, - year )
+d2 <- dplyr::select( d2, - year )
+
+d <- merge( d1, d2, by="tractid" )
+d <- merge( d, md, by="tractid" )
+
+# filter rural districts
+d <- dplyr::filter( d, urban == "urban" )
+
+d <- dplyr::select( d, tractid, 
+                    mhmval00, mhmval12, 
+                    hinc00, 
+                    hu00, vac00, own00, rent00, h30old00,
+                    empclf00, clf00, unemp00, prof00,  
+                    dpov00, npov00,
+                    ag25up00, hs00, col00, 
+                    pop00.x, nhwht00, nhblk00, hisp00, asian00,
+                    cbsa, cbsaname )
+
+
+d <- 
+  d %>%
+  dplyr::mutate( p.white = 100 * nhwht00 / pop00.x,
+                 p.black = 100 * nhblk00 / pop00.x,
+                 p.hisp = 100 * hisp00 / pop00.x, 
+                 p.asian = 100 * asian00 / pop00.x,
+                 p.hs = 100 * (hs00+col00) / ag25up00,
+                 p.col = 100 * col00 / ag25up00,
+                 p.prof = 100 * prof00 / empclf00,
+                 p.unemp = 100 * unemp00 / clf00,
+                 p.vacant = 100 * vac00 / hu00,
+                 mhv.change.00.to.10 = mhmval12 - mhmval00,
+                 p.mhv.change = 100 * (mhmval12 - mhmval00) / mhmval00,
+                 pov.rate = 100 * npov00 / dpov00 )
+
+
+# adjust 2000 home values for inflation 
+mhv.00 <- d$mhmval00 * INFLATION_RATE 
+mhv.10 <- d$mhmval12
+
+# change in MHV in dollars
+mhv.change <- mhv.10 - mhv.00
+
+
+# drop low 2000 median home values
+# to avoid unrealistic growth rates.
+#
+# tracts with homes that cost less than
+# $10,000 are outliers
+mhv.00[ mhv.00 < 10000 ] <- NA
+
+# change in MHV in percent
+mhv.growth <- 100 * ( mhv.change / mhv.00 )
+
+d$mhv.00 <- mhv.00
+d$mhv.10 <- mhv.10
+d$mhv.change <- mhv.change
+d$mhv.growth <- mhv.growth 
+
+# create mini data frame
+df <- data.frame( MedianHomeValue2000=mhv.00, 
+                  MedianHomeValue2010=mhv.10, 
+                  MHV.Change.00.to.10=mhv.change,
+                  MHV.Growth.00.to.12=mhv.growth )
+
+# average growth in median home value for the city
+cbsa_stats_df <- 
+  d %>%
+  dplyr::group_by( cbsaname ) %>%
+  dplyr::summarize( metro.mhv.change = median( mhv.change, na.rm=T ),
+                    metro.mhv.growth = 100 * median( mhv.growth, na.rm=T ) ) %>%
+  dplyr::ungroup() 
+
+# End of Lab 04 Functions-------------------------------------------------------
+
